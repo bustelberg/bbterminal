8ea20077b6f0ff0e019c94e14e8422e69867d2a5
reinier-bustelberg <reinier@bustelberg.nl>
2026-04-02 14:27:44 +0200

    prod

--- backend/main.py
+++ backend/main.py
@@ -39,6 +39,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://bbterminal.vercel.app",
+        "https://bbterminal-api.vercel.app",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
